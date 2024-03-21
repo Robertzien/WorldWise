6239cea521e7367a77fcb9ac2469d2c71eb75eee
robert <robertziengs03@gmail.com>
2024-03-21 16:15:58 +0100

    aanpassingen

--- script.js
+++ script.js
@@ -4,6 +4,32 @@ document.addEventListener('DOMContentLoaded', function() {
   const closeButton = document.getElementById('close-button');
   const overlayMenu = document.querySelectorAll('.category');
   const quizButton = document.querySelector('.quiz-button');
+  let _startX = 0,
+  _startY = 0,
+  _scrollTop = 0,
+  _scrollLeft = 0;
+
+document.onmousedown = OnMouseDown;
+document.onmouseup = OnMouseUp;
+
+function OnMouseDown(event) {
+    document.onmousemove = OnMouseMove;
+    _startX = event.clientX;
+    _startY = event.clientY;
+    _scrollTop = document.documentElement.scrollTop;
+    _scrollLeft = document.documentElement.scrollLeft;
+}
+
+function OnMouseMove(event) {
+    window.scrollTo({
+        left: _scrollLeft + (_startX - event.clientX),
+        top: _scrollTop + (_startY - event.clientY)
+    });
+}
+
+function OnMouseUp() {
+    document.onmousemove = null;
+}
 
   const questions = [
     {
